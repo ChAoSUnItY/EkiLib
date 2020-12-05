5358ec494ff78d69f567a421c08dd50e11ce06e8
KYLE <minecraft.kyle.train@gmail.com>
2020-12-05 23:43:26 +0800

    1.0.17 quick fix

--- src/main/java/com/chaos/eki_lib/objects/blocks/base/LightableBlock.java
+++ src/main/java/com/chaos/eki_lib/objects/blocks/base/LightableBlock.java
@@ -1,23 +1,48 @@
 package com.chaos.eki_lib.objects.blocks.base;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.world.IBlockReader;
+
 public class LightableBlock extends BaseBlock {
     public LightableBlock(Properties properties, int light) {
         super(setLightLevel(properties, light));
+    }
+
+    @Override
+    public VoxelShape getRenderShape(BlockState state, IBlockReader worldIn, BlockPos pos) {
+        return getEmptyRenderShape();
     }
 
     private static Properties setLightLevel(Properties p, int l) {
         return p.setLightLevel(s -> l);
     }
 
+    private static VoxelShape getEmptyRenderShape() {
+        return VoxelShapes.empty();
+    }
+
     public static class LightableHorizontalBlock extends HorizontalBaseBlock {
         public LightableHorizontalBlock(Properties properties, int light) {
             super(setLightLevel(properties, light));
+        }
+
+        @Override
+        public VoxelShape getRenderShape(BlockState state, IBlockReader worldIn, BlockPos pos) {
+            return getEmptyRenderShape();
         }
     }
 
     public static class LightableDirectionalBlock extends DirectionalBaseBlock {
         public LightableDirectionalBlock(Properties properties, int light) {
             super(setLightLevel(properties, light));
+        }
+
+        @Override
+        public VoxelShape getRenderShape(BlockState state, IBlockReader worldIn, BlockPos pos) {
+            return getEmptyRenderShape();
         }
     }
 }
